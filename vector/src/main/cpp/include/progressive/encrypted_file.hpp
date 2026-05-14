05478bac105b765083c6e49e32ad1ec6a6b0ccbc
Anton Maurer <developer@maureranton.com>
2026-05-14 14:21:05 +0100

    Fix encrypted_file.hpp: remove const from isValid() declarations

--- vector/src/main/cpp/include/progressive/encrypted_file.hpp
+++ vector/src/main/cpp/include/progressive/encrypted_file.hpp
@@ -30,7 +30,7 @@ struct EncryptedFileKey {
     // Original Kotlin (EncryptedFileKey.kt:isValid):
     //   alg == "A256CTR" && ext == true && keyOps contains encrypt+decrypt
     //   && kty == "oct" && k is not blank
-    bool isValid() const;
+    bool isValid();
 };
 
 struct EncryptedFileInfo {
@@ -45,7 +45,7 @@ struct EncryptedFileInfo {
     // Original Kotlin (EncryptedFileInfo.kt:isValid):
     //   url not blank, key.isValid(), iv not blank,
     //   hashes contains "sha256", v == "v2"
-    bool isValid() const;
+    bool isValid();
 };
 
 // Parse an EncryptedFileKey from a JSON object.
